6a164c0416d9ac0be06a37a84e9c5877fc4cfa38
a.elena <mssparker91@gmail.com>
2016-04-28 16:36:02 +0300

    module8 task2

--- src/gallery.js
+++ src/gallery.js
@@ -1,97 +1,98 @@
+/**
+ * @fileoverview Компонента фотогалереи
+ */
+
 'use strict';
 
 var domUtils = require('./dom-utils');
 
-var photogallery = document.querySelector('.photogallery');
-var galleryImagesArray = document.querySelectorAll('.photogallery-image > img');
+/** @constructor */
+var Gallery = function() {
+  var self = this;
 
-var galleryContainer = document.querySelector('.overlay-gallery');
-var galleryPreview = galleryContainer.querySelector('.overlay-gallery-preview');
-var galleryBtnClose = galleryContainer.querySelector('.overlay-gallery-close');
-var galleryBtnPrev = galleryContainer.querySelector('.overlay-gallery-control-left');
-var galleryBtnNext = galleryContainer.querySelector('.overlay-gallery-control-right');
-var galleryPreviewTotal = galleryContainer.querySelector('.preview-number-total');
-var galleryPreviewCurrent = galleryContainer.querySelector('.preview-number-current');
+  var galleryContainer = document.querySelector('.overlay-gallery');
+  var galleryPreview = galleryContainer.querySelector('.overlay-gallery-preview');
+  var galleryBtnClose = galleryContainer.querySelector('.overlay-gallery-close');
+  var galleryBtnPrev = galleryContainer.querySelector('.overlay-gallery-control-left');
+  var galleryBtnNext = galleryContainer.querySelector('.overlay-gallery-control-right');
+  var galleryPreviewTotal = galleryContainer.querySelector('.preview-number-total');
+  var galleryPreviewCurrent = galleryContainer.querySelector('.preview-number-current');
 
-/** @constant {number} */
-var ESC = 27;
+  /** @constant {number} */
+  var ESC = 27;
 
-var currentImage;
-var numberImage;
-var createImage = new Image(450, 450);
+  var currentImage;
+  var numberImage;
+  var createImage = new Image(450, 450);
 
-/** @type {Array.<string>} */
-var galleryImages = [];
+  /** @type {Array.<string>} */
+  var galleryImages = [];
 
-var galleryImagesArrayLength = galleryImages.length;
+  var galleryImagesArrayLength = galleryImages.length;
 
-var getGallery = function(srcArray) {
-  galleryImages = srcArray;
-  galleryImagesArrayLength = galleryImages.length;
-  galleryPreviewTotal.textContent = galleryImagesArrayLength;
-  currentImage = galleryPreview.appendChild(createImage);
+  this.getGallery = function(srcArray) {
+    galleryImages = srcArray;
+    galleryImagesArrayLength = galleryImages.length;
+    galleryPreviewTotal.textContent = galleryImagesArrayLength;
+    currentImage = galleryPreview.appendChild(createImage);
+  };
+
+  this.onDocumentKeyDown = function(evt) {
+    if (evt.keyCode === ESC) {
+      self.hideGallery();
+    }
+  };
+
+  this.onCloseClick = function() {
+    self.hideGallery();
+  };
+
+  this.showPrevImage = function() {
+    if (numberImage > 0) {
+      numberImage--;
+      self.changeImage(numberImage);
+    }
+  };
+
+  this.showNextImage = function() {
+    if (numberImage < galleryImagesArrayLength - 1) {
+      numberImage++;
+      self.changeImage(numberImage);
+    }
+  };
+
+  this.changeImage = function(numberCurrent) {
+    numberCurrent = numberImage;
+    currentImage.src = galleryImages[numberCurrent];
+    galleryPreviewCurrent.textContent = numberCurrent + 1;
+
+    domUtils.toggleElementVisibility(galleryBtnPrev, numberCurrent === 0);
+    domUtils.toggleElementVisibility(galleryBtnNext, numberCurrent === galleryImagesArrayLength - 1);
+  };
+
+  this.hideGallery = function() {
+    galleryBtnPrev.removeEventListener('click', this.showPrevImage);
+    galleryBtnNext.removeEventListener('click', this.showNextImage);
+    galleryBtnClose.removeEventListener('click', this.onCloseClick);
+    window.removeEventListener('keydown', this.onDocumentKeyDown);
+
+    domUtils.toggleElementVisibility(galleryContainer, true);
+  };
+
+  /** @param {number} imageId*/
+  this.showGallery = function(imageId) {
+    numberImage = imageId;
+    self.changeImage(numberImage);
+
+    galleryBtnPrev.addEventListener('click', this.showPrevImage);
+    galleryBtnNext.addEventListener('click', this.showNextImage);
+    galleryBtnClose.addEventListener('click', this.onCloseClick);
+    window.addEventListener('keydown', this.onDocumentKeyDown);
+
+    domUtils.toggleElementVisibility(galleryContainer, false);
+  };
 };
 
-var onDocumentKeyDown = function(evt) {
-  if (evt.keyCode === ESC) {
-    hideGallery();
-  }
-};
-
-var onCloseClick = function() {
-  hideGallery();
-};
-
-var showPrevImage = function() {
-  if (numberImage > 0) {
-    numberImage--;
-    changeImage(numberImage);
-  }
-};
-
-var showNextImage = function() {
-  if (numberImage < galleryImagesArrayLength - 1) {
-    numberImage++;
-    changeImage(numberImage);
-  }
-};
-
-var changeImage = function(numberCurrent) {
-  numberCurrent = numberImage;
-  currentImage.src = galleryImages[numberCurrent];
-  galleryPreviewCurrent.textContent = numberCurrent + 1;
-
-  domUtils.toggleElementVisibility(galleryBtnPrev, numberCurrent === 0);
-  domUtils.toggleElementVisibility(galleryBtnNext, numberCurrent === galleryImagesArrayLength - 1);
-};
-
-var hideGallery = function() {
-  galleryBtnPrev.removeEventListener('click', showPrevImage);
-  galleryBtnNext.removeEventListener('click', showNextImage);
-  galleryBtnClose.removeEventListener('click', onCloseClick);
-  window.removeEventListener('keydown', onDocumentKeyDown);
-
-  domUtils.toggleElementVisibility(galleryContainer, true);
-};
-
-/** @param {number} imageId*/
-var showGallery = function(imageId) {
-  numberImage = imageId;
-  changeImage(numberImage);
-
-  galleryBtnPrev.addEventListener('click', showPrevImage);
-  galleryBtnNext.addEventListener('click', showNextImage);
-  galleryBtnClose.addEventListener('click', onCloseClick);
-  window.addEventListener('keydown', onDocumentKeyDown);
-
-  domUtils.toggleElementVisibility(galleryContainer, false);
-};
-
-module.exports = {
-  getGallery: getGallery,
-  showGallery: showGallery,
-  galleryImagesArray: galleryImagesArray,
-  photogallery: photogallery
-};
+module.exports = new Gallery();
 
 
--- src/main.js
+++ src/main.js
@@ -7,11 +7,14 @@ require('./reviews');
 
 var gallery = require('./gallery');
 var domUtils = require('./dom-utils');
+
+var photogallery = document.querySelector('.photogallery');
+var galleryImagesArray = document.querySelectorAll('.photogallery-image > img');
 var imageId;
 
-gallery.getGallery(domUtils.getSrcArray(gallery.galleryImagesArray, domUtils.imgHandler.setDataId));
+gallery.getGallery(domUtils.getSrcArray(galleryImagesArray, domUtils.imgHandler.setDataId));
 
-gallery.photogallery.addEventListener('click', function(evt) {
+photogallery.addEventListener('click', function(evt) {
   evt.preventDefault();
   if (evt.target.tagName === 'IMG') {
     imageId = parseInt(domUtils.imgHandler.getDataId(evt.target), 10);
